6906e1414f8ebfaa5b55476dabeb921e27b5dc22
Marko Radenkovic <markowashere@gmail.com>
2022-02-18 13:00:42 +0100

    Created new pycharm project

---
+++ bot-application/main.py
@@ -0,0 +1,16 @@
+# Import following modules and check how they work
+# Selenium
+# PyAutoGui
+# PyGetWindow
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')
+
+
+# Program Entry Point
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
